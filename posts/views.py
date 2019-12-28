f83b6e66096d602ec0383c42017bd8d2b504105a
Illya Artemchuk <ilyalinkis@gmail.com>
2019-12-28 00:13:46 -0800

    First Commit

---
+++ posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer, UserSerializer
+from .permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
+
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly, )
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
